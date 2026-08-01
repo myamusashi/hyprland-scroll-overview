febcd16e26e45d098f12d21105ef575e59f97867
jwiseuk <jamesrhysellis@live.com>
2026-08-01 20:50:53 +0100

    fix: allow reopening during close animation

--- scrollOverview.cpp
+++ scrollOverview.cpp
@@ -4909,6 +4909,23 @@ void CScrollOverview::close() {
     finishClose(FINALWORKSPACE, FINALWINDOW);
 }
 
+bool CScrollOverview::isClosing() const {
+    return closing;
+}
+
+void CScrollOverview::reopen() {
+    if (!closing)
+        return;
+
+    scale->setCallbackOnEnd({});
+    closeApplied = false;
+    setClosing(false);
+    activateSubmapIfConfigured();
+    emitFullscreenVisibilityState(Desktop::focusState()->window(), true);
+    *scale = ScrollOverview::Config::getScale();
+    damage();
+}
+
 void CScrollOverview::onPreRender() {
     if (pMonitor)
         pMonitor->m_solitaryClient.reset();
@@ -5139,9 +5156,11 @@ void CScrollOverview::setClosing(bool closing_) {
     if (closing) {
         transferSharedStateOwnership();
         inputFramePending = false;
+        if (scrollingPanPointerDown)
+            endScrollingPan();
+        releaseTopLayerPointerButtons(Time::millis(Time::steadyNow()));
         clearDragPending();
         restoreSubmapIfActive();
-        releaseInputListeners();
     } else
         applyWorkspaceAnimationOverrides();
 }
